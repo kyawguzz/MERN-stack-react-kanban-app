10bae601925fec55cab272136a42a628166fad71
Kyaw Zin Htet <kyawzinhtet@Kyaws-MacBook-Pro.local>
2024-02-20 15:10:34 +0630

    update vercel file

--- client/src/api/axiosClient.js
+++ client/src/api/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import queryString from 'query-string'
 
-const baseUrl = 'mern-stack-react-kanban-app-api.vercel.app/api/v1/'
+const baseUrl = 'https://mern-stack-react-kanban-app-api.vercel.app/api/v1/'
 // const baseUrl = 'http://127.0.0.1:5000/api/v1/'
 const getToken = () => localStorage.getItem('token')
 
